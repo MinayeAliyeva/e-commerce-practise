fe2bb5b62d50b89a8fb492abae57d955e9c325f3
MinayeAliyeva <7s346pu@code.edu.az>
2024-04-25 00:10:57 +0400

    sidebar test

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -13,10 +13,15 @@ import useMobile from "./useMobile";
 import MobileVersion from "./MobileVersion";
 import MenuBar from "./MenuBar";
 import SearchButton from "../Buttons/Search";
+import SideBar from "../test/SideBar";
+import { useState } from "react";
 
 export default function PrimarySearchAppBar() {
+  const [open, setOpen] = useState(false);
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
   const state = useMobile();
-
   const {
     menuId,
     mobileMenuId,
@@ -31,88 +36,95 @@ export default function PrimarySearchAppBar() {
   } = React.useMemo(() => state);
 
   return (
-		<Box sx={{ flexGrow: 1 }}>
-			<AppBar position='static'>
-    
-				<Toolbar>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
           <Box sx={{ flexGrow: 1 }}>
-
-					<Typography
-						variant='h6'
-						noWrap
-						component='div'
-						sx={{ display: { xs: "none", sm: "block" } }}>
-						Flows
-					</Typography>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ display: { sm: "block" } }}
+            >
+              Flows
+            </Typography>
           </Box>
 
-					<Box sx={{ flexGrow: 1 }}>
-					   <MenuBar />
+          <Box sx={{ flexGrow: 1 }}>
+            <MenuBar />
           </Box>
-         
 
-					<Box sx={{ display: { xs: "none", md: "flex" } }}>
-						<IconButton
-							size='large'
-							aria-label='show 4 new mails'
-							color='inherit'>
-							{/* <Badge badgeContent={4} color="error">
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            <IconButton
+              size="large"
+              aria-label="show 4 new mails"
+              color="inherit"
+            >
+              {/* <Badge badgeContent={4} color="error">
                 <MailIcon />
               </Badge> */}
-						</IconButton>
-						<IconButton
-							size='large'
-							aria-label='show 17 new notifications'
-							color='inherit'>
-							<Badge badgeContent={17} color='error'>
-								<NotificationsIcon />
-							</Badge>
-						</IconButton>
-						<IconButton
-							size='large'
-							edge='end'
-							aria-label='account of current user'
-							aria-controls={menuId}
-							aria-haspopup='true'
-							onClick={handleProfileMenuOpen}
-							color='inherit'>
-							<AccountCircle />
-						</IconButton>
-					</Box>
-					<Box sx={{ display: { xs: "flex", md: "none" } }}>
-						<IconButton
-							size='large'
-							aria-label='show more'
-							aria-controls={mobileMenuId}
-							aria-haspopup='true'
-							onClick={handleMobileMenuOpen}
-							color='inherit'>
-							<MoreIcon />
-						</IconButton>
-					</Box>
+            </IconButton>
+            <IconButton
+              size="large"
+              aria-label="show 17 new notifications"
+              color="inherit"
+            >
+              <Badge badgeContent={17} color="error">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              size="large"
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+          </Box>
+          <Box sx={{ display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </Box>
 
-					<IconButton
-						size='large'
-						edge='start'
-						color='inherit'
-						aria-label='open drawer'
-						sx={{ mr: 2 }}>
-						<MenuIcon style={{ margin: " 0 10px", fontSize: "30px" }} />
-					</IconButton>
-				</Toolbar>
-			</AppBar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon
+              onClick={() => setOpen(!open)}
+              sx={{ display: { xs: "flex", md: "none" } }}
+            />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <SideBar toggleDrawer={toggleDrawer} open={open} />
 
-			<MobileVersion
-				isMenuOpen={isMenuOpen}
-				handleMenuClose={handleMenuClose}
-				anchorEl={anchorEl}
-				menuId={menuId}
-				mobileMenuId={mobileMenuId}
-				mobileMoreAnchorEl={mobileMoreAnchorEl}
-				isMobileMenuOpen={isMobileMenuOpen}
-				handleMobileMenuClose={handleMobileMenuClose}
-				handleProfileMenuOpen={handleProfileMenuOpen}
-			/>
-		</Box>
-	);
+      <MobileVersion
+        isMenuOpen={isMenuOpen}
+        handleMenuClose={handleMenuClose}
+        anchorEl={anchorEl}
+        menuId={menuId}
+        mobileMenuId={mobileMenuId}
+        mobileMoreAnchorEl={mobileMoreAnchorEl}
+        isMobileMenuOpen={isMobileMenuOpen}
+        handleMobileMenuClose={handleMobileMenuClose}
+        handleProfileMenuOpen={handleProfileMenuOpen}
+      />
+    </Box>
+  );
 }
