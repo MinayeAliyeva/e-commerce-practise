e049312d35330b72a81fe0503e4109ea62dd544e
MinayeAliyeva <7s346pu@code.edu.az>
2024-04-25 09:04:56 +0400

    added shopping card

--- client/src/pages/home/shopping/index.jsx
+++ client/src/pages/home/shopping/index.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import ShoppingCard from '../../../components/Cards/shopping-card'
-
+import React from "react";
+import ShoppingCard from "../../../components/Cards/shopping-card";
+// ,flexWrap:'wrap'
 const ShoppingSection = () => {
   return (
-    <section style={{height:'100vh', background:'red'}}>
-        <ShoppingCard/>
+    <section
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        overflow: "hidden",
+        gap: "10px",
+        justifyContent: "center",
+      }}
+    >
+      <ShoppingCard />
+      <ShoppingCard />
+      <ShoppingCard />
+      <ShoppingCard />
+      <ShoppingCard />
     </section>
-  )
-}
+  );
+};
 
-export default ShoppingSection
+export default ShoppingSection;
